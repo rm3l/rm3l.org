ba24483245fa21f225cdcea175d214661dff17f9
Armel S <armel@rm3l.org>
2019-12-28 22:06:36 +0100

    Enable Google Analytics, via the 'gatsby-plugin-google-analytics' Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -179,6 +179,25 @@ module.exports = {
                 addUncaughtPages: true,
             },
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              // Here goes your tracking ID
+              trackingId: 'UA-103780308-1',
+              // Puts tracking script in the head instead of the body
+              head: true,
+              // IP anonymization for GDPR compliance
+              anonymize: true,
+              // Disable analytics for users with `Do Not Track` enabled
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+            //   exclude: ['/preview/**'],
+              // Specifies what percentage of users should be tracked
+              sampleRate: 100,
+              // Determines how often site speed tracking beacons will be sent
+              siteSpeedSampleRate: 10,
+            },
+          },
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
