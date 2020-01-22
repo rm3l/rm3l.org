c3e15dc555b1bc7e06ab90f18f0a717490da075e
Armel S <armel@rm3l.org>
2020-01-22 23:30:45 +0100

    Fix Axe Accessibility tests in Cypress

--- cypress/e2e/a11y.spec.js
+++ cypress/e2e/a11y.spec.js
@@ -1,21 +1,24 @@
 /// <reference types="Cypress" />
 
 describe(`Accessibility checks`, () => {
-    beforeEach(() => {
+
+    it(`Has no detectable a11y violations on load`, () => {
         cy.visit(`/`)
         cy.injectAxe()
         cy.wait(500)
-    })
-    it(`Has no detectable a11y violations on load`, () => {
         cy.checkA11y()
     })
+
     it(`Navigates to page 2 and checks for accessibility violations`, () => {
         cy.visit(`/page/2`)
+        cy.injectAxe()
         cy.wait(500)
         cy.checkA11y()
     })
+
     it(`Navigates to Portfolio and checks for accessibility violations`, () => {
         cy.visit(`/portfolio`)
+        cy.injectAxe()
         cy.wait(500)
         cy.checkA11y()
     })
