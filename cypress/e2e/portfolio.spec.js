f21851d00864fc459e5440d14a693d35f609cfb3
Armel Soro <armel@rm3l.org>
2020-12-23 00:03:38 +0100

    Add test that ensures all portfolio card images are resolvable

--- cypress/e2e/portfolio.spec.js
+++ cypress/e2e/portfolio.spec.js
@@ -2,13 +2,32 @@
 describe(`Portfolio page`, () => {
     beforeEach(() => {
         cy.visit(`/portfolio`)
-        cy.wait(500)
+        cy.wait(2000)
     })
 
     it(`Works and ALL selected by default`, () => {
         cy.contains(`h1`, `Projects`)
         cy.get(`button[data-filter="ALL"]`)
             .should(`have.css`, `background-color`, `rgb(255, 99, 71)`)
+    })
+
+    it(`ensures all portfolio card images are resolvable`, () => {
+        cy.get(`.card__image img`)
+            .should(`be.visible`)
+            .each(($el) => {
+                cy.wrap($el)
+                    .and(($img) => {
+                    // "naturalWidth" and "naturalHeight" are set when the image loads
+                        expect($img[0].naturalWidth).to.be.greaterThan(0)
+                    })
+                    .invoke(`attr`, `src`)
+                    .then((imgSrc) => {
+                        cy.log(`imgSrc=[${imgSrc}]`)
+                        cy.request(imgSrc).then((resp) => {
+                            expect(resp.status).to.eq(200)
+                        })
+                    })
+            })
     })
 
     it(`Clicking on filter buttons works and highlights the right button`, () => {
