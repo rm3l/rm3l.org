a48d6966d6d6720d25e889c8f2669741402253e3
Armel Soro <armel@rm3l.org>
2021-03-26 00:22:03 +0100

    fix: Fix case where no cover image has been set in the CMS settings

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -62,7 +62,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
 
                 <div className="viewport-top">
                     {/* The main header section on top of the screen */}
-                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${config.sitePublicationCoverUrl || site.cover_image})` } }}>
+                    <header className="site-head" style={{ backgroundImage: `url(${config.sitePublicationCoverUrl || site.cover_image})` }}>
                         <div className="container">
                             <div className="site-mast">
                                 <div className="site-mast-left">
