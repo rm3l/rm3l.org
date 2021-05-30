d1ca4cdd8b70bb1f2e9c9d7f89d55d90cfe71a48
Armel Soro <armel@rm3l.org>
2021-05-30 23:47:03 +0200

    fix(#782): Fix issue causing icons to be huge on page initial load
    Workaround suggested in [1]
    
    [1] https://github.com/FortAwesome/react-fontawesome/issues/134

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -10,6 +10,14 @@ import config from '../../utils/siteConfig'
 // Styles
 import '../../styles/app.css'
 
+// Workaround to problem with icons being huge huge first load prior to resizing
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+// See https://github.com/FortAwesome/react-fontawesome/issues/134
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core"
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+fontAwesomeConfig.autoAddCss = false
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
